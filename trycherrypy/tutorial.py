553ffcebb66136c0767bc82887ebea29775bc141
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-02-07 15:45:14 +0100

    Learn how to submit a form

--- trycherrypy/tutorial.py
+++ trycherrypy/tutorial.py
@@ -3,6 +3,7 @@
 Tutorial 1: A basic web application
 Tutorial 2: Different URLs lead to different functions
 Tutorial 3: My URLs have parameters
+Tutorial 4: Submit this form
 """
 import random
 import string
@@ -13,7 +14,16 @@ import cherrypy
 class StringGenerator:
     @cherrypy.expose
     def index(self):
-        return "Hello world!"
+        return """<html>
+            <head></head>
+            <body>
+                <form method="get" action="generate">
+                    <label for="length">Length</label>
+                    <input type="text" value="8" name="length" />
+                    <button type="submit">Give it now!</button>
+                </form>
+            </body>
+        </html>"""
     
     @cherrypy.expose
     def generate(self, length=8):
