bffa31850d09e54c3a065e71b626a06c2c614665
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-02-07 16:37:57 +0100

    Learn how make the web app RESTful

--- trycherrypy/tutorial.py
+++ trycherrypy/tutorial.py
@@ -6,51 +6,43 @@ Tutorial 3: My URLs have parameters
 Tutorial 4: Submit this form
 Tutorial 5: Track my-end-user's activity
 Tutorial 6: What about my javascripts, CSS and images?
+Tutorial 7: Give us a REST
 """
-import os
-import os.path
+# import os
+# import os.path
 import random
 import string
 
 import cherrypy
 
 
-class StringGenerator:
-    @cherrypy.expose
-    def index(self):
-        return """<html>
-            <head>
-                <link href="/static/css/style.css" rel="stylesheet">
-            </head>
-            <body>
-                <form method="get" action="generate">
-                    <label for="length">Length</label>
-                    <input type="text" value="8" name="length" />
-                    <button type="submit">Give it now!</button>
-                </form>
-            </body>
-        </html>"""
+@cherrypy.expose
+class StringGeneratorWebService:
+    @cherrypy.tools.accept(media="text/plain")
+    def GET(self):
+        return cherrypy.session["mystring"]
     
-    @cherrypy.expose
-    def generate(self, length=8):
-        text = ''.join(random.sample(string.hexdigits, int(length)))
+    def POST(self, length=8):
+        text = "".join(random.sample(string.hexdigits, int(length)))
         cherrypy.session["mystring"] = text
         return text
     
-    @cherrypy.expose
-    def display(self):
-        return cherrypy.session["mystring"]
+    def PUT(self, other_txt):
+        cherrypy.session["mystring"] = other_txt
+
+    def DELETE(self):
+        cherrypy.session.pop("mystring", None)
 
 
 if __name__ == "__main__":
     conf = {
         "/": {
+            "request.dispatch": cherrypy.dispatch.MethodDispatcher(),
             "tools.sessions.on": True,
-            "tools.staticdir.root": os.path.abspath(os.path.dirname(__file__)),
-        },
-        "/static": {
-            "tools.staticdir.on": True,
-            "tools.staticdir.dir": "./public",
+            "tools.response_headers.on": True,
+            "tools.response_headers.headers": [
+                ("Context-Type", "text/plain"),
+            ],
         },
     }
-    cherrypy.quickstart(StringGenerator(), "/", conf)
+    cherrypy.quickstart(StringGeneratorWebService(), "/", conf)
