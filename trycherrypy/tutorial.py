8ffc2646dd4482ea71077ed94967a404942d2260
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-02-07 17:10:38 +0100

    integrate the use of Ajax in the web app

--- trycherrypy/tutorial.py
+++ trycherrypy/tutorial.py
@@ -7,14 +7,21 @@ Tutorial 4: Submit this form
 Tutorial 5: Track my-end-user's activity
 Tutorial 6: What about my javascripts, CSS and images?
 Tutorial 7: Give us a REST
+Tutorial 8: Make it smoother with Ajax
 """
-# import os
-# import os.path
+import os
+import os.path
 import random
 import string
 
 import cherrypy
 
+
+class StringGenerator:
+    @cherrypy.expose
+    def index(self):
+        fname= os.path.join(os.path.dirname(__file__), "public", "index.html")
+        return open(fname)
 
 @cherrypy.expose
 class StringGeneratorWebService:
@@ -37,12 +44,19 @@ class StringGeneratorWebService:
 if __name__ == "__main__":
     conf = {
         "/": {
-            "request.dispatch": cherrypy.dispatch.MethodDispatcher(),
             "tools.sessions.on": True,
-            "tools.response_headers.on": True,
-            "tools.response_headers.headers": [
-                ("Context-Type", "text/plain"),
-            ],
+            "tools.staticdir.root": os.path.abspath(os.path.dirname(__file__)),
         },
+        "/generator": {
+            "request.dispatch": cherrypy.dispatch.MethodDispatcher(),
+            "tools.response_headers.on": True,
+            "tools.response_headers.headers": [("Content-Type", "text/plain")],
+        },
+        "/static": {
+            "tools.staticdir.on": True,
+            "tools.staticdir.dir": "./public",
+        }
     }
-    cherrypy.quickstart(StringGeneratorWebService(), "/", conf)
+    webapp = StringGenerator()
+    webapp.generator = StringGeneratorWebService()
+    cherrypy.quickstart(webapp, "/", conf)
