65f0c65b1ac51f8ba10111d63e57ae537a4061c3
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-02-07 15:28:50 +0100

    Learn how to point to different URLs in cherrypy web app.

--- trycherrypy/tutorial.py
+++ trycherrypy/tutorial.py
@@ -1,15 +1,23 @@
 """Cherrypy tutorial.
 
 Tutorial 1: A basic web application
+Tutorial 2: Different URLs lead to different functions
 """
+import random
+import string
+
 import cherrypy
 
 
-class HelloWorld:
+class StringGenerator:
     @cherrypy.expose
     def index(self):
         return "Hello world!"
     
+    @cherrypy.expose
+    def generate(self):
+        return "".join(random.sample(string.hexdigits, 8))
+
 
 if __name__ == "__main__":
-    cherrypy.quickstart(HelloWorld())
+    cherrypy.quickstart(StringGenerator())
